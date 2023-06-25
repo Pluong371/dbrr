5f4f9e9857c86690ffc0a911124954e4044e93ce
luong9cm <pluong371@gmail.com>
2023-06-25 12:28:12 +0700

    change main

--- Main.java
+++ Main.java
@@ -1,16 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Fruit_0023;
 
 /**
- *
+ * 
  * @author admin
  */
 public class Main {
-
+    
     public static void main(String[] args) {
         ManageFruit manage = new ManageFruit();
         Utility utility = new Utility();
